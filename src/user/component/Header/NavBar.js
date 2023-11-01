7f65748c2980e9655729cdaa4ff23b10231cb092
vohaiphuc <vohaiphuc@gmail.com>
2023-11-01 14:02:13 +0700

    add font awesome

--- src/user/component/Header/NavBar.js
+++ src/user/component/Header/NavBar.js
@@ -1,10 +1,12 @@
+import { faGlobe } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 
 export default function NavBar() {
     return (
         <>
             <button>Cho thuê chổ ở qua Airbnb</button>
-
+            <FontAwesomeIcon icon={faGlobe} />
         </>
     )
 }
