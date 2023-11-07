e17e672881f06db4715f4717867ae9c47fc4e6f3
vohaiphuc <vohaiphuc@gmail.com>
2023-11-07 15:40:15 +0700

    search-bar still bug

--- src/user/api/api.js
+++ src/user/api/api.js
@@ -3,3 +3,7 @@ import { https } from "./config";
 export const userServ = {
     login: (info) => https.post(`/auth/signin`, info),
 }
+
+export const viTriServ = {
+    get: () => https.get("/vi-tri")
+}
