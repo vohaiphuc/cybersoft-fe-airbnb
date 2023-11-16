8a15071411dce05ab968da2e421fc9af37bad68c
vohaiphuc <vohaiphuc@gmail.com>
2023-11-16 09:33:39 +0700

    fix menumobile

--- src/user/component/MenuMobile/MenuMobile.js
+++ src/user/component/MenuMobile/MenuMobile.js
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { useWindowWidth } from '@react-hook/window-size'
 import React, { useEffect, useState } from 'react'
 import { userRoute } from '../../route/userRoute'
-import { NavLink, useLocation, useNavigate } from 'react-router-dom'
+import { useLocation, useNavigate } from 'react-router-dom'
 import { POPUP_NAME } from '../../constants/popup'
 import { useDispatch, useSelector } from 'react-redux'
 import { setPopup } from '../../redux/popupSlice'
@@ -11,8 +11,8 @@ import "./style.scss"
 
 export default function MenuMobile() {
     const windowWidth = useWindowWidth()
-    const isMobile = windowWidth <= 640
-    const [show, setShow] = useState(true);
+    const isMobile = windowWidth < 1000
+    const [show, setShow] = useState(isMobile);
     const navigate = useNavigate()
     const dispatch = useDispatch()
     const { user } = useSelector(s => s.userSlice)
@@ -33,9 +33,11 @@ export default function MenuMobile() {
 
             lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // For Mobile or negative scrolling
         }
-
         if (isMobile) {
             document.addEventListener("scroll", handleScroll)
+        }
+        return () => {
+            document.removeEventListener("scroll", handleScroll)
         }
     }, [isMobile])
 
@@ -43,7 +45,7 @@ export default function MenuMobile() {
         {
             icon: faHome,
             title: "Trang chủ",
-            active: urlPath == userRoute.home.path ? "active" : "",
+            active: (urlPath == userRoute.home.path) ? "active" : "",
             onClick: () => { navigate(userRoute.home.path) },
         },
         {
@@ -55,7 +57,7 @@ export default function MenuMobile() {
         {
             icon: faUser,
             title: user ? "Tài khoản" : "Đăng nhập",
-            active: urlPath == userRoute.account.path ? "active" : "",
+            active: (urlPath == userRoute.account.path) ? "active" : "",
             onClick: () => {
                 user
                     ? navigate(userRoute.account.path)
@@ -64,7 +66,7 @@ export default function MenuMobile() {
         },
     ]
 
-    return (
+    return isMobile && (
         <div className={`menu-mobile w-full fixed transition-all ${show ? 'bottom-0' : '-bottom-20'} left-0 bg-white flex items-center justify-center space-x-5 shadow-2xl z-50 border-t-[1px] py-3`}>
             {buttonList.map((item, index) => (
                 <div className={`flex flex-col items-center ${item.active}`} key={index} onClick={item.onClick}>
